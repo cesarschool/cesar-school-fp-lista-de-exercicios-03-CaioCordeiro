7d86265688b2611971edf2a4db260da18621b763
Ícaro Ribeiro <icaro.nribeiro@gmail.com>
2018-04-05 18:10:22 -0300

    Update questao_8.py

--- questoes/questao_8.py
+++ questoes/questao_8.py
@@ -1,6 +1,15 @@
-## QUESTÃO 1 ##
-# Faça um programa que receba cinco inteiros e diga qual deles é o maior e qual o menor.
+## QUESTÃO 8 ##
+# Escreva um programa que leia uma data do usuário e calcule seu sucessor imediato.
+# Por exemplo, se o usuário inserir valores que representem 2013-11-18, seu programa 
+# deve exibir uma mensagem indicando que o dia imediatamente após 2013-11-18 é 
+# 2013-11-19. Se o usuário inserir valores que representem 2013-11-30, o programa deve 
+# indicar que o dia seguinte é 2013-12-01. Se o usuário inserir valores que representem 
+# 2013-12-31 então o programa deve indicar que o dia seguinte é 2014-01-01. A data 
+# será inserida em formato numérico com três instruções de entrada separadas; 
+# uma para o ano, uma para o mês e uma para o dia. Certifique-se de que o seu programa 
+# funciona corretamente para anos bissextos.
 ##
+
 
 ##
 # A sua resposta da questão deve ser desenvolvida dentro da função main()!!! 
@@ -9,9 +18,9 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 1")
+    print("questao 8")
+
+
     
-
-
 if __name__ == '__main__':
     main()
