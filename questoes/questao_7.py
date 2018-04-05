b9082d385985103cac29634a1ec93801af23dc5c
Ícaro Ribeiro <icaro.nribeiro@gmail.com>
2018-04-05 18:09:30 -0300

    Update questao_7.py

--- questoes/questao_7.py
+++ questoes/questao_7.py
@@ -1,6 +1,16 @@
-## QUESTÃO 1 ##
-# Faça um programa que receba cinco inteiros e diga qual deles é o maior e qual o menor.
+## QUESTÃO 7 ##
+# A maioria dos anos tem 365 dias. No entanto, o tempo necessário para a Terra orbitar 
+# o Sol é na verdade um pouco mais que isso. Como resultado, um dia extra, 29 de fevereiro, 
+# está incluído em alguns anos para corrigir essa diferença. Esses anos são referidos como 
+# anos bissextos. As regras para determinar se um ano é ou não um ano bissexto seguem:
+# • Qualquer ano que seja divisível por 400 é um ano bissexto.
+# • Dos anos restantes, qualquer ano que seja divisível por 100 não é um ano bissexto.
+# • Dos anos restantes, qualquer ano que seja divisível por 4 é um ano bissexto.
+# • Todos os outros anos não são anos bissextos.
+# Escreva um programa que leia um ano do usuário e exiba uma mensagem indicando 
+# se é ou não um ano bissexto.
 ##
+
 
 ##
 # A sua resposta da questão deve ser desenvolvida dentro da função main()!!! 
@@ -9,9 +19,9 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 1")
+    print("questao 7")
+
+
     
-
-
 if __name__ == '__main__':
     main()
