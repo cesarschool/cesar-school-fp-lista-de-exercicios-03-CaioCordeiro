18c05dd0bec6dee8d3d01aaaaec958dcbab101fb
Ícaro Ribeiro <icaro.nribeiro@gmail.com>
2018-05-16 11:42:32 -0300

    Update questao_5.py

--- questoes/questao_5.py
+++ questoes/questao_5.py
@@ -1,6 +1,40 @@
 ## QUESTÃO 5 ##
-# Escreva um programa que leia as medidas dos lados de um triângulo e escreva se ele é 
-# Equilátero, Isósceles ou Escaleno.
+#
+# Crie uma implementação da cifra rotacional, às vezes também chamada de cifra de César.
+# 
+# A cifra de César é uma simples cifra de deslocamento que se baseia na transposição 
+# de todas as letras do alfabeto usando uma chave inteira entre 0 e 26. 
+# O uso da chave 0 ou 26 sempre produzirá a mesma saída devido à aritmética modular. 
+# A letra é deslocada para tantos valores quanto o valor da chave.
+#
+# A notação geral para cifras rotacionais é ROT + <chave>. A cifra rotacional mais comumente usada é a ROT13.
+#
+# Um ROT13 no alfabeto latino seria o seguinte:
+# 	Normal:  abcdefghijklmnopqrstuvwxyz
+#	Cifrado: nopqrstuvwxyzabcdefghijklm
+#
+# Exemplos:
+#	Entrada: ROT5 omg 
+#          Saída: trl
+#	Entrada: ROT0 c 
+#          Saída: c
+#	Entrada: ROT26 Cool 
+#          Saída: Cool
+#	Entrada: ROT13 The quick brown fox jumps over the lazy dog. 
+#          Saída: Gur dhvpx oebja sbk whzcf bire gur ynml qbt.
+#	Entrada: ROT13 Gur dhvpx oebja sbk whzcf bire gur ynml qbt. 
+#          Saída: The quick brown fox jumps over the lazy dog.
+#
+# Se um valor de entrada inválido for digitado, a seguinte mensagem 
+# deve ser impressa: 'Erro'. 
+# Entradas inválidas podem ser entradas que contém códigos de rotações 
+# inválidos ou mensagens contendo caracteres que não estão no alfabeto. 
+# Exemplos:
+# 	Entrada: RARA abc Saída: Erro
+# 	Entrada: ROT5 c99 Saída: Erro
+#
+# As entradas devem ser sempre compostas por ROT + <chave> + ' ' + 'mensagem', 
+# ou seja, a cifra rotacional e a mensagem a ser cifrada separados por vírgula.
 ##
 
 
