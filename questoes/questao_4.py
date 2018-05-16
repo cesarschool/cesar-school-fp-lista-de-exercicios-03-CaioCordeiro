97d3d3dc17b740be55529ba085ca79efb6bb93cf
Ícaro Ribeiro <icaro.nribeiro@gmail.com>
2018-05-16 11:41:31 -0300

    Update questao_4.py

--- questoes/questao_4.py
+++ questoes/questao_4.py
@@ -1,9 +1,29 @@
 ## QUESTÃO 4 ##
-# Escreva um programa para aprovar o empréstimo bancário para compra de uma casa. 
-# O programa deve perguntar o valor da casa a comprar, o salário e a quantidade de anos 
-# a pagar. O valor da prestação mensal não pode ser superior a 30% do salário. Calcule o 
-# valor da prestação como sendo o valor da casa a comprar dividido pelo número de 
-# meses a pagar.
+#
+# Você foi solicitado a escrever um programa para classificar tuplas (nome, idade, altura) 
+# por ordem crescente, em que nome é uma string, idade e altura são números. As tuplas
+# são inseridas pelo console. O critério de classificação é:
+#
+# 1: Ordenar com base no nome;
+# 2: Em seguida, classifique com base na idade;
+# 3: Em seguida, classifique por pontuação.
+#
+# A prioridade é essa nome > idade > pontuação.
+# 
+# Se as seguintes tuplas são dadas como entrada para o programa:
+# Tom 19,80
+# João, 20,90
+# Jony, 17,91
+# Jony, 17,93
+# Json, 21,85
+#
+# Então, a saída do programa deve ser:
+# [('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21 ',' 85 '), (' Tom ',' 19 ',' 80 ')]
+#
+# Dicas:
+# As entradas devem ser lidas do console até que um valor vazio seja digitado. 
+# A saída deve ser impressa no formato apresentado acima.
+# Valores inválidos (fora do formato esperado para uma tupla) devem ser descartados.
 ##
 
 
