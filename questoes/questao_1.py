5fd236e6022201f47cdb65048bfaebcb4081cd11
Ícaro Ribeiro <icaro.nribeiro@gmail.com>
2018-05-16 11:37:58 -0300

    Update questao_1.py

--- questoes/questao_1.py
+++ questoes/questao_1.py
@@ -1,5 +1,24 @@
 ## QUESTÃO 1 ##
-# Faça um programa que receba cinco inteiros e diga qual deles é o maior e qual o menor.
+#
+# Um site exige que os usuários insiram nome de usuário e senha para se registrarem. 
+# Escreva um programa para verificar se a senha digitada pelos usuários é forte o suficiente.
+# A seguir estão os critérios para verificar a senha:
+#
+# 1. Pelo menos uma letra entre [a-z]
+# 2. Pelo menos 1 número entre [0-9]
+# 3. Pelo menos uma letra entre [A-Z]
+# 4. Pelo menos 1 caractere de [$ # @]
+# 5. Comprimento mínimo da senha: 6
+# 6. Comprimento máximo da senha: 12
+#
+# Seu programa deve aceitar uma sequência de senhas separadas por vírgula e as verificará 
+# de acordo com os critérios acima. As senhas que correspondem aos critérios devem ser 
+# impressas, separadas por uma vírgula.
+# Exemplo
+# Se as seguintes senhas forem fornecidas como entrada para o programa:
+# ABd1234 @ 1, um F1 #, 2w3E *, 2We3345
+# Então, a saída do programa deve ser:
+# ABd1234 @ 1
 ##
 
 ##
