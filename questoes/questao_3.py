b590d8bdb1b5a5746b566ad842f6fc31cb1084dd
CaioCordeiro <37188299+CaioCordeiro@users.noreply.github.com>
2018-05-20 11:54:11 -0300

    Update questao_3.py

--- questoes/questao_3.py
+++ questoes/questao_3.py
@@ -38,7 +38,43 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 3")
+    c = 0
+
+    b = 0
+
+    e = 0
+
+    d = 0
+
+    def direcao(x):
+        global c
+        global b
+        global e
+        global d
+        xs = x.split()
+        if xs[0] == 'CIMA':
+            c += int(xs[1])
+        if xs[0] == 'BAIXO':
+            b += int(xs[1])
+        if xs[0] == 'ESQUERDA':
+            e += int(xs[1])
+        if xs[0] == 'DIREITA':
+            d += int(xs[1])
+
+
+    while True:
+        x = input().upper()
+        if x == '':
+            break
+        else:
+            direcao(x)
+            continue
+
+    z = ((c - b)**2 + (d - e)**2)**(1/2)
+    z = round(z)
+    print(z)
+
+
 
 
     
