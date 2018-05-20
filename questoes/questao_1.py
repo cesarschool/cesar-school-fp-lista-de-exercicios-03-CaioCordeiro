ab5132a9cff2517a1aeabea4323eff5f8da2a2ee
CaioCordeiro <37188299+CaioCordeiro@users.noreply.github.com>
2018-05-20 11:50:34 -0300

    Update questao_1.py

--- questoes/questao_1.py
+++ questoes/questao_1.py
@@ -28,8 +28,54 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 1")
-    
+    senha = input('')
+    senha = senha.split(',')
+    senhasok = []
+    def alpha(snh):
+        for x in snh :
+            if x.isalpha() == True :
+                return True
+
+    def special(snh):
+        carac1 = '@'
+        carac2 = '#'
+        carac3 = '$'
+        for x in snh :
+            if x == carac1 or x== carac2 or x== carac3:
+                return True
+
+    def maiusculo(snh):
+        for x in snh:
+            if x.isupper() == True:
+                return True
+
+    def minusculo(snh):
+        for x in snh :
+            if x.islower() == True :
+                return True
+    def num(snh):
+        for x in snh :
+            if x.isnumeric() == True :
+                return True
+    def tamanho(snh):
+        lenx = len(snh)
+        if lenx >= 6 and lenx <= 12 :
+            return True
+
+
+    def final(snh):
+        if alpha(snh) and num(snh) and special(snh) and maiusculo(snh) and minusculo(snh) and tamanho(snh) :
+            return True
+
+    for i in senha :
+        if final(i) == True :
+            senhasok.append(i)
+        else:
+            continue
+    print(','.join(senhasok))
+
+
+
 
 
 if __name__ == '__main__':
